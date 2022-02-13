a1405b0b4aa192872ea319fc40135c9f951e3587
AProgrammablePhoenix <alexgf27550@gmail.com>
2022-02-13 11:04:32 +0100

    1.14.14

--- fission reactor theorin't.js
+++ fission reactor theorin't.js
@@ -14,6 +14,8 @@ var UDExp, PHWR, MSR;
 quaternaryEntries = [];
 var isCurrencyVisible = (index) => index == 8;
 var init = () => {
+    quaternaryEntries.width = 200;
+
     currency1 = theory.createCurrency("U","U");
     currency2 = theory.createCurrency("Np","Np");
     currency3 = theory.createCurrency("Pu","Pu");
@@ -389,14 +391,14 @@ var getSecondaryEquation = () => theory.latexSymbol + "=\\max P^{0.76}";
 var getQuaternaryEntries = () => {
     if (quaternaryEntries.length == 0)
     {
-        quaternaryEntries.push(new QuaternaryEntry("U", null));
-        quaternaryEntries.push(new QuaternaryEntry("Np", null));
-        quaternaryEntries.push(new QuaternaryEntry("Pu", null));
-        quaternaryEntries.push(new QuaternaryEntry("Am", null));
-        quaternaryEntries.push(new QuaternaryEntry("Cm", null));
-        quaternaryEntries.push(new QuaternaryEntry("Bk", null));
-        quaternaryEntries.push(new QuaternaryEntry("Cf", null));
-        quaternaryEntries.push(new QuaternaryEntry("Es", null));
+        quaternaryEntries.push(new QuaternaryEntry("_{U}", null));
+        quaternaryEntries.push(new QuaternaryEntry("_{Np}", null));
+        quaternaryEntries.push(new QuaternaryEntry("_{Pu}", null));
+        quaternaryEntries.push(new QuaternaryEntry("_{A_m}", null));
+        quaternaryEntries.push(new QuaternaryEntry("_{C_m}", null));
+        quaternaryEntries.push(new QuaternaryEntry("_{Bk}", null));
+        quaternaryEntries.push(new QuaternaryEntry("_{Cf}", null));
+        quaternaryEntries.push(new QuaternaryEntry("_{Es}", null));
     }
     quaternaryEntries[0].value=currency1.value;
     quaternaryEntries[1].value=currency2.value;
